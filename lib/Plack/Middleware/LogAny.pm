e092910896173854fecb36676761f4b524799215
Michael Alan Dorman <mdorman@ironicdesign.com>
2012-11-02 10:40:16 -0400

    Simplest possible implementation
    Based somewhat on Plack::Middleware::Log4perl, this is the simplest
    implementation I could come up with---it presumes that all real
    configuration will be done elsewhere; that is, it makes no attempt to
    install an adapter and actually get logging happening.

--- lib/Plack/Middleware/LogAny.pm
+++ lib/Plack/Middleware/LogAny.pm
@@ -1,6 +1,71 @@
+package Plack::Middleware::LogAny;
+# ABSTRACT: Use Log::Any to handle logging from your Plack app
+
+use Log::Any qw{};
+use Plack::Util::Accessor qw{category logger};
+use parent qw{Plack::Middleware};
 use strict;
 use warnings;
-package Plack::Middleware::LogAny;
-# ABSTRACT: Use Log::Any to control logging from your Plack app
+
+=head1 SYNOPSIS
+
+  builder {
+      enable "LogAny", category => "plack";
+      $app;
+  }
+
+=head1 DESCRIPTION
+
+LogAny is a L<Plack::Middleware> component that allows you to use
+L<Log::Any> to handle the logging object, C<psgix.logger>.
+
+It really tries to be the thinnest possible shim, so it doesn't handle
+any configuration beyond setting the category to which messages from
+plack might be logged.
+
+=head1 CONFIGURATION
+
+=over 4
+
+=item category
+
+The C<Log::Any> category to send logs to. Defaults to C<''> which
+means it send to the root logger.
+
+=back
+
+=method prepare_app
+
+This method initializes the logger using the category that you
+(optionally) set.
+
+=head1 SEE ALSO
+
+L<Log::Any>
+
+=cut
+
+sub prepare_app {
+    my ($self) = @_;
+    $self->logger (Log::Any->get_logger (category => $self->category || ''));
+}
+
+=method call
+
+Actually handles making sure the logger is invoked.
+
+=cut
+
+sub call {
+    my ($self, $env) = @_;
+
+    $env->{'psgix.logger'} = sub {
+        my $args = shift;
+        my $level = $args->{level};
+        $self->logger->$level ($args->{message});
+    };
+
+    $self->app->($env);
+}
 
 1;
